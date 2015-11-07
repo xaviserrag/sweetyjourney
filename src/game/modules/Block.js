7edb1925c54f99959c80831c930f014a8e803170
Adria Rios <adrianrios89@gmail.com>
2015-11-07 11:42:42 +0100

    Refactor and clean block

--- src/game/modules/Block.js
+++ src/game/modules/Block.js
@@ -1,13 +1,5 @@
 'use strict';
 var Block = function Block(params) {
-    params.name = 'blocked';
-
-
-
-
-
-    console.log('params', params);
-
     //VARS
     var self = this,
         type = params.type,
@@ -26,35 +18,38 @@ var Block = function Block(params) {
     this.col = params.col;
     this.row = params.row;
 
-    switch (type){
-        case BLOCKED:
-            name = 'blocked';
-            break;
-        case HORIZONTAL_BLOCK:
-            name = 'block_hor';
-            break;
-        case VERTICAL_BLOCK:
-            name = 'bloc_dev';
-            break;
-    }
 
-    Phaser.Sprite.call(this, params.game, params.x, params.y, name);
+    var createBlock = function createBlock(){
+        switch (type){
+            case BLOCKED:
+                name = 'blocked';
+                break;
+            case HORIZONTAL_BLOCK:
+                name = 'block_hor';
+                break;
+            case VERTICAL_BLOCK:
+                name = 'bloc_dev';
+                break;
+        }
+        Phaser.Sprite.call(self, params.game, params.x, params.y, name);
+        self.game.add.existing(self);
+    };
 
-    console.log('type', type);
+    var initBlockMovement = function initBlockMovement(){
+        if (type === HORIZONTAL_BLOCK){
+            self.inputEnabled = true;
+            self.input.allowVerticalDrag = false;
+            self.events.onDragStop.add(onDragStop, this);
+        }else if (type === VERTICAL_BLOCK){
+            self.inputEnabled = true;
+            self.input.allowHorizontalDrag = false;
+            self.events.onDragStop.add(onDragStop, this);
+        }
+    };
 
     var init = function init(){
-        self.game.add.existing(self);
-        self.inputEnabled = true;
-        self.events.onDragStop.add(onDragStop, this);
-        if (type === HORIZONTAL_BLOCK){
-            var floor = new Phaser.Rectangle(0, 0, range, width);
-            self.input.enableDrag(false,false,false,255,floor);
-            self.input.allowVerticalDrag = false;
-        }else if (type === VERTICAL_BLOCK){
-            var floor = new Phaser.Rectangle(0, 0, height, range);
-            self.input.allowHorizontalDrag = false;
-            self.input.enableDrag();
-        }
+        createBlock();
+        initBlockMovement();
     };
 
     var onDragStop = function onDragStop (sprite, pointer) {
