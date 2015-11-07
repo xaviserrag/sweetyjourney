0f658ae4221aea8449a4ecbaaaeb9dee97040a10
Xavi Serra <xaviserrag@gmail.com>
2015-11-07 10:51:46 +0100

    (DIRTY) grid with block

--- src/game/modules/Grid.js
+++ src/game/modules/Grid.js
@@ -1,21 +1,37 @@
 'use strict';
 
+var config = require('../config/main');
 var Block = require('./Block');
+
 
 var Grid = function Grid(params) {
     Phaser.Group.call(this, params.game, params.x, params.y, params.name);
 
     var self = this;
+    var types = {
+        0: 'empty',
+        1: 'blocked',
+        2: 'horizontal',
+        3: 'vertical'
+    };
+
 
     this.theoricalGrid = [];
 
     var createBlock = function createBlock(col, row, cellPosition) {
+        var type = types[config.level[0].grid[col][row]];
+
+        if (type === 'empty') {
+            return;
+        }
+
         var blockInfo = {
             x: 180 * col,
             y: 180 * row,
-            name: 'block',
+            type: type,
             game: self.game
         };
+
 
         return new Block(blockInfo);
 
--- src/game/states/preload.js
+++ src/game/states/preload.js
@@ -9,7 +9,7 @@ Preload.prototype = {
     preload: function () {
         this.load.tilemap('map', 'assets/tiles/tiles.json', null, Phaser.Tilemap.TILED_JSON);
         this.load.image('tiles', 'assets/tiles/tiles.png');
-        this.load.image('block', 'assets/images/block.png');
+        this.load.image('blocked', 'assets/images/block.png');
         this.load.image('block1', 'assets/images/block1.png');
         this.load.image('block2', 'assets/images/block2.png');
         this.load.image('playButton', 'assets/images/play_button.png');
@@ -17,7 +17,7 @@ Preload.prototype = {
 
     },
     create: function () {
-        this.game.state.start('menu');
+        this.game.state.start('play');
     }
 };
 
