ad2ee146d477e65190d1a90b78724a8a5d9929dc
Xavier Serra <xaviserrag@gmail.com>
2024-09-12 13:09:16 +0200

    Trigger deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@
 'use strict';
 var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
 
+
 var mountFolder = function (connect, dir) {
     return connect.static(require('path').resolve(dir));
 };
